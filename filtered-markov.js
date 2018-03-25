a7a3c51e5d88c411d68346057fa2ba918446321f
Ryan1729 <Ryan1729@gmail.com>
2018-03-25 14:35:12 -0600

    hook up passthrough version of generator
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

---
+++ filtered-markov.js
@@ -0,0 +1,10 @@
+var FilteredMarkov = (function() {
+    "use strict";
+    var generate = function(inputString) {
+        return inputString;
+    };
+    
+    return {
+        generate: generate,
+    }
+}())
